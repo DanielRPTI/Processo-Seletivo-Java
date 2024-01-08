4c82cd4f5b140bc6f1177ddbc02b3a2d38fa0197
DanielRPTI <drjunior988@hotmail.com>
2024-01-08 14:29:36 -0300

    Seleção de candidatos e geração de valor pretendido randomicamente

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,12 +1,34 @@
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
         Scanner sc =  new Scanner(System.in).useLocale(Locale.US);
-        analisarCadidato(1900.0);
-        analisarCadidato(2200.0);
-        analisarCadidato(2000.0);
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos() {
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Daniela", "Joao"};
+        
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido );
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato" + candidato + " Foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     static void analisarCadidato(double salarioPretendido) {
